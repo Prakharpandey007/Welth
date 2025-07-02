62e043febc6b9c762679b8fceedc90a4ee12e737
prakhar <prakharharshit.2003@gmail.com>
2025-07-02 22:06:47 +0530

    feat:Add the landing page

--- middleware.js
+++ middleware.js
@@ -17,9 +17,11 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/',                    // 👈 Explicit match for landing page
+    '/dashboard(.*)',
+    '/account(.*)',
+    '/transaction(.*)',
+    '/((?!.*\\..*|_next|_static).*)', // Catch-all
+    '/(api|trpc)(.*)',
   ],
 };
