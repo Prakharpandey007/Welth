f4fe1c7d31cc03e30067f0f39ca9725813147e43
prakhar <prakharharshit.2003@gmail.com>
2025-07-05 22:59:27 +0530

    feat:Add the account-card accounts-server-file

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,12 @@
+import { getUserAccounts } from '@/actions/dashboard'
 import {CreateAccountDrawer} from '@/components/create-account-drawer'
 import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import AccountCard from './_components/account-card'
 
-const DashboardPage = () => {
+export default async function DashboardPage(){
+const accounts=await getUserAccounts()
   return (
     <div className='px-5'>
       {/* Budget Progree  */}
@@ -18,13 +21,11 @@ const DashboardPage = () => {
             </CardContent>
           </Card>
         </CreateAccountDrawer>
-        {/* {accounts.length > 0 &&
+        {accounts.length > 0 &&
           accounts?.map((account) => (
             <AccountCard key={account.id} account={account} />
-          ))} */}
+          ))}
       </div>
     </div>
   )
 }
-
-export default DashboardPage
--- components/AppShell.jsx
+++ components/AppShell.jsx
@@ -1,11 +1,12 @@
 
 import Header from "@/components/header";
-
+import { Toaster } from "sonner";
 export default function AppShell({ children }) {
   return (
     <>
       <Header />
       <main className="min-h-screen">{children}</main>
+      <Toaster richColors />
       <footer className="bg-blue-50 py-12">
         <div className="container mx-auto px-4 text-center text-gray-600">
           <p>Made with 💗 by IIIT-RANCHI</p>
